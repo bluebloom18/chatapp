624fd3bff2f315a841a6adfee424d78c94ad329f
Lien Ber Luen <lienbl@yahoo.com>
2021-12-22 21:09:16 +0800

    Add message events

--- server/server.js
+++ server/server.js
@@ -13,6 +13,27 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=> {
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from:'John',
+        text: 'see you',
+        createdAt: 1234
+
+    });
+    /*
+    socket.emit('newEmail', {
+        from: 'john@tan.com',
+        text: 'How are you',
+        createdAt: 123
+    });
+
+    socket.on('createEmail', (newEmail)=>{
+        console.log('createEmail', newEmail);
+    });
+    */
+
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage', message);
+    });
 
     socket.on('disconnect', ()=> {
         console.log('User disconnected');
